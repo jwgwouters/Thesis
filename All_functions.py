b4a60472c04300ba5b492add3fa3f91bb054705e
jwgwouters <59361415+jwgwouters@users.noreply.github.com>
2020-09-25 12:27:51 +0200

    Added NA

--- All_functions.py
+++ All_functions.py
@@ -1,7 +1,7 @@
 #Functions file
 #
 
-def calc_eucl_ugs_gini (city, EPSG, UA_data, eucl_dist, ftprnt_type, tags):
+def calc_eucl_ugs_gini (city, EPSG, UA_data, eucl_dist, ftprnt_type, tags, id):
     import osmnx as ox
     import geopandas as gpd
     import pandas as pd
@@ -16,23 +16,23 @@ def calc_eucl_ugs_gini (city, EPSG, UA_data, eucl_dist, ftprnt_type, tags):
     city_shp_pop = city_shp_cut[city_shp_cut['Pop2012'] > 0]
     # extract osm landuse
     parks_landuse_tags = tags
-    x = ox.footprints_from_place("Maastricht", footprint_type= ftprnt_type)
+    x = ox.footprints_from_place(city, footprint_type= ftprnt_type)
     # only select relevant green space tags from the landuse extraction
-    parks = x[x['landuse'].isin(parks_landuse_tags)]
+    parks = x[x[ftprnt_type].isin(parks_landuse_tags)]
     # convert the green spaces to RD new
     parks_RD = parks.to_crs(EPSG)
     # only keep relevant columns
-    parks_RD_clean = parks_RD[["landuse", "geometry"]].copy()
+    parks_RD_clean = parks_RD[[ftprnt_type, "geometry"]].copy()
     parks_RD_clean["park_area"] = parks_RD_clean.area
     pop_buffer = gpd.GeoDataFrame(city_shp_pop, geometry=city_shp_pop.buffer(eucl_dist)).copy()
     # only keep relevant columns
-    pop_buffer_clean = pop_buffer[["IDENT", "Pop2012", "geometry"]]
+    pop_buffer_clean = pop_buffer[[id, "Pop2012", "geometry"]]
     # identify the parks within the buffers
     park_buffer_join = gpd.sjoin(parks_RD_clean, pop_buffer_clean, how="inner", op="intersects")
     #create new dataframe with the total park area per ident(unique pop polygon id)
-    tot_df = park_buffer_join.groupby("IDENT")["park_area"].sum()
+    tot_df = park_buffer_join.groupby(id)["park_area"].sum()
     ## add the population column
-    tot_df = pd.merge(tot_df, pop_buffer_clean[["IDENT", "Pop2012"]], on="IDENT", how="inner")
+    tot_df = pd.merge(tot_df, pop_buffer_clean[[id, "Pop2012"]], on=id, how="inner")
     #Calculate the gini
     gini_table = tot_df
     gini_table["pop_frctn"] = tot_df["Pop2012"] / (tot_df["Pop2012"].sum())
@@ -50,21 +50,156 @@ def calc_eucl_ugs_gini (city, EPSG, UA_data, eucl_dist, ftprnt_type, tags):
     return gini, gini_table
 
 
-def calc_netwanly_ugs_gini (city, EPGS, UA_data, dist):
-    import osmnx as ox
+def calc_na_ugs_gini (city, EPGS, UA_data, dist, ftprnt_type, tags, id):
     import geopandas as gpd
+    import networkx as nx
+    import osmnx as ox
+    import numpy as np
     import pandas as pd
-    # Extract city boundary and road network
-    city_boundary = ox.geocode_to_gdf(city, buffer_dist= 500)
-    roads_city = ox.graph_from_place(city, retain_all= True, network_type='walk', buffer_dist=500, which_result=2)
-    nodes, city_roads = ox.graph_to_gdfs(roads_city)
+    from descartes import PolygonPatch
+    from shapely.geometry import Point, LineString, Polygon
 
+    network_type = 'all'
+    trip_times = [(dist / 100)]
+    travel_speed = 6  # walking speed in km/hour
 
+    #download streetnetwork
+    G = ox.graph_from_place(city, network_type=network_type)
+    # extract osm landuse
+    x = ox.footprints_from_place(city, footprint_type=ftprnt_type)
+    # only select relevant green space tags from the landuse extraction
+    parks = x[x[ftprnt_type].isin(tags)]
 
+    # transform pop, park and road network to RD_new
+    G_tf = ox.project_graph(G, to_crs=EPGS)
+    pop_tf = UA_data.to_crs(EPGS)
+    parks_RD = parks.to_crs(EPGS)
 
+    # only keep relevant columns
+    parks_RD_clean = parks_RD[[ftprnt_type, "geometry"]].copy()
+    parks_RD_clean["park_area"] = parks_RD_clean.area
 
+    # Extract city boundary
+    city_boundary = ox.geocode_to_gdf(city)
+    city_boundary_RD = city_boundary.to_crs(EPGS)
 
-def plot_graph_gini(data1, label1, data2,label2, data3, label3, filename):
+    # cut the data by the city boundary
+    city_shp_cut = gpd.overlay(city_boundary_RD, pop_tf, how='intersection')
+
+    # select only the pop polygons
+    pop_poly = city_shp_cut[city_shp_cut['Pop2012'] > 0]
+
+    # create a tuple with the x an y coordinates of the pop polygon centroids
+    coords_cen = np.vstack([pop_poly.centroid.x, pop_poly.centroid.y]).T
+    coords_cen_tp = tuple(map(tuple, coords_cen))
+
+    # add an edge attribute for time in minutes required to traverse each edge
+    meters_per_minute = travel_speed * 1000 / 60  # km per hour to m per minute
+    for u, v, k, data in G.edges(data=True, keys=True):
+        data['time'] = data['length'] / meters_per_minute
+    print("starting NA poly generation")
+    def make_iso_polys(G, center_node, edge_buff=25, node_buff=50, infill=False, ):
+        isochrone_polys = []
+        for trip_time in sorted(trip_times, reverse=True):
+            subgraph = nx.ego_graph(G, center_node, radius=trip_time, distance='time')
+
+            node_points = [Point((data['x'], data['y'])) for node, data in subgraph.nodes(data=True)]
+            nodes_gdf = gpd.GeoDataFrame({'id': list(subgraph.nodes)}, geometry=node_points)
+            nodes_gdf = nodes_gdf.set_index('id')
+
+            edge_lines = []
+            for n_fr, n_to in subgraph.edges():
+                f = nodes_gdf.loc[n_fr].geometry
+                t = nodes_gdf.loc[n_to].geometry
+                edge_lookup = G.get_edge_data(n_fr, n_to)[0].get('geometry', LineString([f, t]))
+                edge_lines.append(edge_lookup)
+
+            n = nodes_gdf.buffer(node_buff).geometry
+            e = gpd.GeoSeries(edge_lines).buffer(edge_buff).geometry
+            all_gs = list(n) + list(e)
+            new_iso = gpd.GeoSeries(all_gs).unary_union
+
+            # try to fill in surrounded areas so shapes will appear solid and blocks without white space inside them
+            if infill:
+                new_iso = Polygon(new_iso.exterior)
+            isochrone_polys.append(new_iso)
+        return isochrone_polys
+
+    def iterate_multiple_coords(the_coordinates):
+        poly_list = []
+        count = 0
+        for i in the_coordinates:
+            count = count + 1
+            # method is euclidean since epgs is unprojected
+            centre_node = ox.get_nearest_node(G_tf, (i[1], i[0]), method='euclidean')
+            # print("starting" + str(i)) # to check
+            # add an edge attribute for time in minutes required to traverse each edge
+            for u, v, k, data in G_tf.edges(data=True, keys=True):
+                data['time'] = data['length'] / meters_per_minute
+            # make the polygons
+            isochrone_polys_iter = make_iso_polys(G_tf,
+                                                  centre_node,
+                                                  edge_buff=25,
+                                                  node_buff=0,
+                                                  infill=False)
+            #add a progress notification
+            if count == round((0.1 * len(the_coordinates))):
+                print("10% complete")
+            if count == round((0.25 * len(the_coordinates))):
+                print("25% complete")
+            if count == round((0.5 * len(the_coordinates))):
+                print("50% complete")
+            if count == round((0.75 * len(the_coordinates))):
+                print("75% complete")
+            if count == round((0.9 * len(the_coordinates))):
+                print("90% complete")
+            # print("done " + str([i])) # to check
+            poly_list.append(isochrone_polys_iter)
+            # print("appended " + str([i])) # to check
+        return (poly_list)
+    #create the na service areas
+    pop_na_poly = iterate_multiple_coords(coords_cen_tp)
+    print("Finish NA poly generation, starting Gini calculation")
+    #create a save command an if statement so if only the tags change it doens't recalculate the buffers
+
+    ## turn shapely geometries list file to geodataframe
+    na_gdf = gpd.GeoDataFrame()
+    for i in range(len(pop_na_poly)):
+        na_gdf = na_gdf.append(gpd.GeoDataFrame(geometry=pop_na_poly[i], crs=EPGS))
+
+    # set same index as the pop_poly
+    na_gdf = na_gdf.set_index(pop_poly.index)
+
+    # rejoin the original pop data  & IDENT to the created buffers
+    na_gdf["Pop2012"] = pop_poly.Pop2012
+    na_gdf['IDENT'] = pop_poly[id]
+
+    # follow the steps as in the euclidean buffer
+    # identify the parks within the na service areaa
+    park_NA_join = gpd.sjoin(parks_RD_clean, na_gdf, how="inner", op="intersects")
+
+    # create new dataframe with the total park area per ident(unique pop polygon id)
+    tot_df = park_NA_join.groupby("IDENT")["park_area"].sum()
+    ## add the population column
+    tot_df = pd.merge(tot_df, na_gdf[["IDENT", "Pop2012"]], on="IDENT", how="inner")
+
+    # Calculate the gini
+    gini_table = tot_df
+    gini_table["pop_frctn"] = tot_df["Pop2012"] / (tot_df["Pop2012"].sum())
+    gini_table["green_frctn"] = tot_df["park_area"] / tot_df["park_area"].sum()
+    gini_table = gini_table.sort_values(["green_frctn"])
+    gini_table["cum_sum_pop_frctn"] = gini_table["pop_frctn"].cumsum()
+    gini_table["cum_sum_green_frctn"] = gini_table["green_frctn"].cumsum()
+    gini_table["prev_cs_g_f"] = gini_table["cum_sum_green_frctn"].shift(1)
+    gini_table["prev_cs_g_f"] = gini_table["prev_cs_g_f"].fillna(0)
+    gini_table["area"] = ((gini_table["cum_sum_green_frctn"] + gini_table["prev_cs_g_f"]) * 0.5) * gini_table[
+        "pop_frctn"]
+    b = gini_table["area"].sum()
+    a = 0.5 - b
+    gini = a / (a + b)
+    return gini, gini_table
+
+def plot_graph_gini(data1, label1, data2,label2, filename):
     import matplotlib.pyplot as plt
     x_base = [0, 1]
     y_base = [0, 1]
@@ -73,8 +208,8 @@ def plot_graph_gini(data1, label1, data2,label2, data3, label3, filename):
              label=label1, linewidth=2, color="red")
     plt.plot(data2["cum_sum_pop_frctn"], data2["cum_sum_green_frctn"],
              label=label2, linewidth=2, color="yellow")
-    plt.plot(data3["cum_sum_pop_frctn"], data3["cum_sum_green_frctn"],
-             label=label3, linewidth=2, color="green")
+    #plt.plot(data3["cum_sum_pop_frctn"], data3["cum_sum_green_frctn"],
+     #        label=label3, linewidth=2, color="green")
     plt.xlabel("Cumulative share of population")
     plt.ylabel("Cumulative share of greenspace")
     plt.title("Gini coefficient")
--- Main.py
+++ Main.py
@@ -27,25 +27,63 @@ projection = 'EPSG:28992'
 
 ## Read shapefile(not possible to create direct download link, since there is a need to login for access)
 city_shp = gpd.read_file('./data/NL505L1_MAASTRICHT/Shapefiles/NL505L1_MAASTRICHT_UA2012.shp')
+Amsterdam_UA = gpd.read_file("./data/NL002L3_AMSTERDAM_013/Data/NL002L3_AMSTERDAM_013.gpkg")
+London_UA = gpd.read_file("./data/UK001L3_LONDON_013/Data/UK001L3_LONDON_013.gpkg")
+Paris_UA = gpd.read_file("./data/FR001L1_PARIS_013/Data/FR001L1_PARIS_013.gpkg")
+
+Bristol_UA = gpd.read_file("./data/UK011L2_BRISTOL_UA2012_revised_021/Data/UK011L2_BRISTOL_UA2012_revised_021.gpkg")
+Essen_UA = gpd.read_file("./data/DE038L1_RUHRGEBIET_013/Data/DE038L1_RUHRGEBIET_013.gpkg")
+Nijmegen_UA = gpd.read_file("./data/NL013L3_NIJMEGEN_UA2012_revised_021/Data/NL013L3_NIJMEGEN_UA2012_revised_021.gpkg")
+
 
 #tags
 parks_tag = ['parks', 'dog_park', 'garden', 'playground', 'nature_reserve']
 landuse_tag = ['grass', 'allotments', 'meadow', 'forest']
 
-## calculate the euclidean distance gini for maastricht and parks
-maastr_eucl_200_gini, maastr_eucl_200_gini_table = funcs.calc_eucl_ugs_gini("Maastricht", 'EPSG:28992', city_shp, 200, 'leisure', parks_tag)
-maastr_eucl_300_gini, maastr_eucl_300_gini_table = funcs.calc_eucl_ugs_gini("Maastricht", 'EPSG:28992', city_shp, 300, 'leisure', parks_tag)
-maastr_eucl_1000_gini, maastr_eucl_1000_gini_table = funcs.calc_eucl_ugs_gini("Maastricht", 'EPSG:28992', city_shp, 1000, 'leisure', parks_tag)
+#calculate Amsterdam NA and Eucl 300m  gini for landuse tags
+amst_na_300_landuse_gini, amst_na_300_landuse_gini_table = funcs.calc_na_ugs_gini("Amsterdam", 'EPSG:28992', Amsterdam_UA, 300, 'landuse', landuse_tag)
+amst_eucl_300_landuse_gini, amst_eucl_300_landuse_gini_table = funcs.calc_eucl_ugs_gini("Amsterdam", 'EPSG:28992', Amsterdam_UA, 300, 'landuse', landuse_tag)
 
-#plot the gini and save to file
-funcs.plot_graph_gini(maastr_eucl_200_gini_table, "Maastricht Euclidean 200m", maastr_eucl_300_gini_table, "Maastricht Euclidean 300m",
-                maastr_eucl_1000_gini_table, "Maastricht Euclidean 1000m", "maastricht_gini")
+#calculate London NA and Eucl 300m  gini for landuse tags
+lond_na_300_landuse_gini, lond_na_300_landuse_gini_table = funcs.calc_na_ugs_gini("London", 'EPSG:27700', London_UA, 300, 'landuse', landuse_tag)
+lond_eucl_300_landuse_gini, lond_eucl_300_landuse_gini_table = funcs.calc_eucl_ugs_gini("London", 'EPSG:27700', London_UA, 300, 'landuse', landuse_tag)
+
+#calculate Paris NA and Eucl 300m  gini for landuse tags
+pari_na_300_landuse_gini, pari_na_300_landuse_gini_table = funcs.calc_na_ugs_gini("Paris", 'EPSG: 27572', Paris_UA, 300, 'landuse', landuse_tag)
+pari_eucl_300_landuse_gini, pari_eucl_300_landuse_gini_table = funcs.calc_eucl_ugs_gini("Paris", 'EPSG: 27572', Paris_UA, 300, 'landuse', landuse_tag)
+
+#calculate Bristol NA and Eucl 300m  gini for landuse tags
+brist_na_800_landuse_gini, brist_na_800_landuse_gini_table = funcs.calc_na_ugs_gini("Bristol", 'EPSG:27700', Bristol_UA, 800, 'landuse', landuse_tag, 'identifier')
+brist_eucl_800_landuse_gini, brist_eucl_800_landuse_gini_table = funcs.calc_eucl_ugs_gini("Bristol", 'EPSG:27700', Bristol_UA, 800, 'landuse', landuse_tag, 'identifier')
+
+#calculate Essen NA and Eucl 300m  gini for landuse tags
+ess_na_800_landuse_gini, ess_na_800_landuse_gini_table = funcs.calc_na_ugs_gini("Essen", 'EPSG:5676', Essen_UA, 800, 'landuse', landuse_tag, id="IDENT")
+ess_eucl_800_landuse_gini, ess_eucl_800_landuse_gini_table = funcs.calc_eucl_ugs_gini("Essen", 'EPSG:5676', Essen_UA, 800, 'landuse', landuse_tag, id="IDENT")
+
+#calculate Nijmegen NA and Eucl 300m  gini for landuse tags
+nijm_na_800_landuse_gini, nijm_na_800_landuse_gini_table = funcs.calc_na_ugs_gini("Nijmegen", 'EPSG:28992', Nijmegen_UA, 800, 'landuse', landuse_tag, 'identifier')
+nijm_eucl_800_landuse_gini, nijm_eucl_800_landuse_gini_table = funcs.calc_eucl_ugs_gini("Nijmegen", 'EPSG:28992', Nijmegen_UA, 800, 'landuse', landuse_tag, id= 'identifier')
 
 
-#Network analysis
+#Plot & save the gini graphs per city
+funcs.plot_graph_gini(brist_eucl_800_landuse_gini_table, "Bristol landuse Euclidean 800m", brist_na_800_landuse_gini_table, "Bristol landuse Network Analysis 800m",
+                      filename='brist_na_eucl_800_gini_landuse')
 
+funcs.plot_graph_gini(ess_eucl_800_landuse_gini_table, "Essen landuse Euclidean 800m", ess_na_800_landuse_gini_table, "Essen landuse Network Analysis 800m",
+                      filename='ess_na_eucl_800_gini_landuse')
 
+funcs.plot_graph_gini(nijm_eucl_800_landuse_gini_table, "Nijmegen landuse Euclidean 800m", nijm_na_800_landuse_gini_table, "Nijmegen landuse Network Analysis 800m",
+                      filename='nijm_na_eucl_800_gini_landuse')
 
+#save gini tables (need to incorporate into function)
+nijm_eucl_800_landuse_gini_table.to_csv("./output/nijmegen_800_eucl_gini_table.csv")
+nijm_na_800_landuse_gini_table.to_csv("./output/nijmegen_800_na_gini_table.csv")
+
+ess_eucl_800_landuse_gini_table.to_csv("./output/essen_800_eucl_gini_table.csv")
+ess_na_800_landuse_gini_table.to_csv("./output/essen_800_na_gini_table.csv")
+
+brist_eucl_800_landuse_gini_table.to_csv("./output/bristol_800_eucl_gini_table.csv")
+brist_na_800_landuse_gini_table.to_csv("./output/bristol_800_na_gini_table.csv")
 
 
 
